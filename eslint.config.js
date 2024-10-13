7741d94919af6e3a1371c5cc541b93ed29a810d6
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-10-12 20:32:32 -0500

    Updating eslint config4

--- eslint.config.js
+++ eslint.config.js
@@ -1,44 +1,15 @@
-const js = require('@eslint/js');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const tsParser = require('@typescript-eslint/parser');
-const prettierPlugin = require('eslint-plugin-prettier');
-const prettierConfig = require('eslint-config-prettier');
-const reactPlugin = require('eslint-plugin-react');
-
-module.exports = [
-  js.configs.recommended,
-  {
-    files: ['**/*.{ts,tsx,js,jsx}'],
-    languageOptions: {
-      parser: tsParser,
+module.exports = {
+  files: ['**/*.js', '**/*.ts'], // Apply to JavaScript and TypeScript files
+  languageOptions: {
+    parserOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
     },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-      react: reactPlugin,
-      prettier: prettierPlugin,
-    },
-    rules: {
-      ...tsPlugin.configs.recommended.rules,
-      semi: ['error', 'always'],
-      'prefer-const': 'error',
-      'no-var': 'error',
-      'no-unused-vars': 'warn',
-      'sort-imports': [
-        'error',
-        {
-          ignoreCase: true,
-          ignoreDeclarationSort: true,
-          memberSyntaxSortOrder: ['none', 'all', 'multiple', 'single'],
-        },
-      ],
-      'react/prop-types': 'off',
-      'prettier/prettier': 'error',
-    },
   },
-  prettierConfig,
-  {
-    ignores: ['cdk.out', 'node_modules', 'dist', 'build'],
+  rules: {
+    semi: ['error', 'always'], // Enforce semicolons
+    quotes: ['error', 'single'], // Enforce single quotes
+    'no-unused-vars': 'warn', // Warn about unused variables
+    'sort-imports': ['error', { ignoreDeclarationSort: true }], // Sort imports
   },
-];
+};
