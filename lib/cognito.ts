55db7508f3afda3b92023df5c64677c536d7a687
adamsulemanji <84648379+adamsulemanji@users.noreply.github.com>
2024-10-24 00:03:52 -0500

    Testing small commit

--- lib/cognito.ts
+++ lib/cognito.ts
@@ -1,18 +1,42 @@
-import { Construct } from 'constructs';
-import * as cdk from 'aws-cdk-lib';
-import * as cognito from 'aws-cdk-lib/aws-cognito';
+import { Construct } from 'constructs'
+import * as cdk from 'aws-cdk-lib'
+import * as cognito from 'aws-cdk-lib/aws-cognito'
 
 export class CognitoConstruct extends Construct {
-  public readonly userPool: cognito.UserPool;
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+    public readonly userPool: cognito.UserPool
+    public readonly userPoolClient: cognito.UserPoolClient
 
-    this.userPool = new cognito.UserPool(this, 'UserPool', {
-      selfSignUpEnabled: true,
-      signInAliases: { email: true },
-      standardAttributes: {
-        email: { required: true, mutable: false },
-      },
-    });
-  }
+    constructor(scope: Construct, id: string) {
+        super(scope, id)
+
+        // ****** Create a Cognito User Pool ******
+        this.userPool = new cognito.UserPool(this, 'CourseMonitoringPool', {
+            selfSignUpEnabled: true,
+            signInAliases: { email: true },
+            standardAttributes: {
+                email: { required: true, mutable: false },
+            },
+            passwordPolicy: {
+                minLength: 8,
+                requireDigits: true,
+                requireUppercase: true,
+                requireLowercase: false,
+                requireSymbols: false,
+            },
+        })
+
+        // ****** Create a Cognito User Pool Client ******
+        this.userPoolClient = new cognito.UserPoolClient(
+            this,
+            'CourseMonitoringAppClient',
+            {
+                userPool: this.userPool,
+                authFlows: {
+                    userPassword: true,
+                    userSrp: true,
+                },
+                preventUserExistenceErrors: true,
+            }
+        )
+    }
 }
--- lib/ddb.ts
+++ lib/ddb.ts
@@ -1,22 +1,30 @@
-import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
-import { Construct } from 'constructs';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb'
+import { Construct } from 'constructs'
 
 export class DynamoDBConstruct extends Construct {
-  public readonly userTable: dynamodb.Table;
-  public readonly classTable: dynamodb.Table;
+    public readonly userTable: dynamodb.Table
+    public readonly classTable: dynamodb.Table
 
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+    constructor(scope: Construct, id: string) {
+        super(scope, id)
 
-    this.userTable = new dynamodb.Table(this, 'UserTable', {
-      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-    });
+        // ****** Create a DynamoDB Table ******
+        this.userTable = new dynamodb.Table(this, 'UserTable', {
+            partitionKey: {
+                name: 'userId',
+                type: dynamodb.AttributeType.STRING,
+            },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        })
 
-    this.classTable = new dynamodb.Table(this, 'ClassTable', {
-      partitionKey: { name: 'classId', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
-    });
-  }
+        // ****** Create a DynamoDB Table ******
+        this.classTable = new dynamodb.Table(this, 'ClassTable', {
+            partitionKey: {
+                name: 'classId',
+                type: dynamodb.AttributeType.STRING,
+            },
+            sortKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
+            billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+        })
+    }
 }
